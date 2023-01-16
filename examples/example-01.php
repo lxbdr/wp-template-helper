f1f9d42a6ac97739162bfbc237376c8c648e4592
Alexander Bader <a.bader@lxbdr.de>
2023-01-16 22:48:17 +0100

    add example

---
+++ examples/example-01.php
@@ -0,0 +1,29 @@
+<?php
+
+$data = [
+    'foo' => 'bar',
+    'foo_url' => 'https://example.com',
+    'style' => 'color: red; font-weight: bold',
+    'img' => 'https://picsum.photos/100/100'
+];
+
+$t = new \Lxbdr\WpTemplateHelper\WpTemplateHelper($data);
+
+?>
+
+    <div class="card">
+        <?php
+        if ($t->notEmpty('img')): ?>
+            <img src="<?php $t->url('img'); ?>" alt="Beautiful picture">
+        <?php
+        endif; ?>
+        <h1 class="card-title"><?php
+            $t('foo'); ?></h1>
+        <p style="<?php
+        $t->attr('style'); ?>">lorem ipsum</p>
+        <a href="<?php
+        $t->url('foo_url'); ?>">Read more</a>
+    </div>
+
+<?php
+// ...
