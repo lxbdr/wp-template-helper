9a72f6e69d120531440dfde4395a4fcc5b68f40d
Alexander Bader <mail@alexanderbader.de>
2024-11-08 11:18:46 +0100

    add generated docs

--- src/Traits/ImgTrait.php
+++ src/Traits/ImgTrait.php
@@ -2,99 +2,151 @@
 
 namespace Lxbdr\WpTemplateHelper\Traits;
 
+/**
+ * Trait ImgTrait
+ *
+ * Provides methods for handling and rendering various types of images in WordPress templates.
+ * Supports basic images, responsive images, and advanced image configurations with custom
+ * styling and layout options.
+ *
+ * @package Lxbdr\WpTemplateHelper\Traits
+ */
 trait ImgTrait
 {
-
-    public function img( string $key, $size = 'full', $atts = '' ) {
-        echo $this->_img( $key, $size, $atts );
+    /**
+     * Outputs an image HTML element based on the provided key.
+     *
+     * @param string $key The key to lookup the image data
+     * @param string|array $size The desired image size (default: 'full')
+     * @param string|array $atts Additional HTML attributes for the img tag
+     * @return void
+     */
+    public function img(string $key, $size = 'full', $atts = '')
+    {
+        echo $this->_img($key, $size, $atts);
     }
 
-    public function _img( string $key, $size = 'full', $atts = '' ): string {
-        $img = $this->getNested( $key );
-        if ( ! $img ) {
+    /**
+     * Generates an image HTML element based on the provided key.
+     * Supports various input formats including image IDs, URLs, and arrays with metadata.
+     *
+     * @param string $key The key to lookup the image data
+     * @param string|array $size The desired image size (default: 'full')
+     * @param string|array $atts Additional HTML attributes for the img tag
+     * @return string The generated HTML for the image
+     */
+    public function _img(string $key, $size = 'full', $atts = ''): string
+    {
+        $img = $this->getNested($key);
+        if (!$img) {
             return '';
         }
 
-        if ( is_numeric( $img ) ) {
+        if (is_numeric($img)) {
             $id = $img;
-
-            return \wp_get_attachment_image( $id, $size, false, $atts );
-        } elseif ( is_array( $img ) ) {
+            return \wp_get_attachment_image($id, $size, false, $atts);
+        } elseif (is_array($img)) {
             $url = $img['url'] ?? null;
-            $alt = $img['alt'] ?? ( is_array( $atts ) ? ( $atts['alt'] ?? '' ) : '' );
-        } elseif ( is_string( $img ) ) {
+            $alt = $img['alt'] ?? (is_array($atts) ? ($atts['alt'] ?? '') : '');
+        } elseif (is_string($img)) {
             $url = $img;
-            $alt = is_array( $atts ) ? ( $atts['alt'] ?? '' ) : '';
+            $alt = is_array($atts) ? ($atts['alt'] ?? '') : '';
         }
 
-        if ( ! isset($url) ) {
+        if (!isset($url)) {
             return '';
         }
 
-        if (! isset($alt)) {
+        if (!isset($alt)) {
             $alt = '';
         }
 
-        if ( is_array( $atts ) ) {
-            unset( $atts['alt'] );
-            $atts = self::_attributes( $atts );
+        if (is_array($atts)) {
+            unset($atts['alt']);
+            $atts = self::_attributes($atts);
         }
 
         ob_start();
         ?>
-        <img src="<?php
-        echo \esc_url( $url ); ?>" alt="<?php
-        echo \esc_attr( $alt ); ?>" <?php
-        echo $atts; ?>>
+        <img src="<?php echo \esc_url($url); ?>" alt="<?php echo \esc_attr($alt); ?>" <?php echo $atts; ?>>
         <?php
-
         return ob_get_clean();
     }
 
-    public function _advancedImg( string $key ) {
-        $group = $this->getNested( $key );
-        if ( ! $group ) {
+    /**
+     * Generates HTML for an advanced image configuration with custom styling and layout options.
+     *
+     * @param string $key The key to lookup the advanced image configuration
+     * @return string The generated HTML
+     */
+    public function _advancedImg(string $key)
+    {
+        $group = $this->getNested($key);
+        if (!$group) {
             return "";
         }
 
-        return $this->getAdvancedImgData( $group );
+        return $this->getAdvancedImgData($group);
     }
 
-    public function advancedImg( string $key ) {
-        echo $this->_advancedImg( $key );
+    /**
+     * Outputs HTML for an advanced image configuration.
+     *
+     * @param string $key The key to lookup the advanced image configuration
+     * @return void
+     */
+    public function advancedImg(string $key)
+    {
+        echo $this->_advancedImg($key);
     }
 
-    public function _responsiveImg( string $key ) {
-        $group = $this->getNested( $key );
+    /**
+     * Generates HTML for a responsive image with multiple sources based on media queries.
+     *
+     * @param string $key The key to lookup the responsive image configuration
+     * @return string The generated HTML
+     */
+    public function _responsiveImg(string $key)
+    {
+        $group = $this->getNested($key);
 
         $base_img = $group['base_img'] ?? null;
 
-        if ( ! $base_img && ! empty( $group ) ) {
+        if (!$base_img && !empty($group)) {
             // try regular img
-            return $this->_img( $key );
+            return $this->_img($key);
         }
 
-        if ( ! $group ) {
+        if (!$group) {
             return "";
         }
 
-        $data = $this->getResponsiveImgData( $group );
-
-        return $this->renderResponsiveImg( $data );
+        $data = $this->getResponsiveImgData($group);
+        return $this->renderResponsiveImg($data);
     }
 
-    public function responsiveImg( string $key ) {
-        echo $this->_responsiveImg( $key );
+    /**
+     * Outputs HTML for a responsive image.
+     *
+     * @param string $key The key to lookup the responsive image configuration
+     * @return void
+     */
+    public function responsiveImg(string $key)
+    {
+        echo $this->_responsiveImg($key);
     }
 
-    protected function getAdvancedImgData( $group ) {
-
-        // todo: classname and style logic should be moved to template
-        // todo: styles would require this class to register wp classes
-
+    /**
+     * Processes advanced image configuration data and prepares it for rendering.
+     *
+     * @param array $group The advanced image configuration data
+     * @return string The processed HTML
+     */
+    protected function getAdvancedImgData($group)
+    {
+        // Handle image sizing classes
         $size = $group['sizing'];
-
-        switch ( $size ) {
+        switch ($size) {
             case 'width-full-height-full':
                 $size = 'lx-img--full-width lx-img--full-height';
                 break;
@@ -107,172 +159,166 @@ trait ImgTrait
             case 'width-auto-height-auto':
             default:
                 $size = '';
-                // default
                 break;
         }
 
-
+        // Process custom dimensions
         $styles = [];
-
         $custom_width = $group['custom_width'];
-        if ( $custom_width ) {
+        if ($custom_width) {
             $styles[] = "--width: {$custom_width}";
         }
         $custom_height = $group['custom_height'];
-        if ( $custom_height ) {
+        if ($custom_height) {
             $styles[] = "--height: {$custom_height}";
         }
+        $styles = implode("; ", $styles);
 
-        $styles = implode( "; ", $styles );
-
+        // Configure display and object-fit properties
         $object_fit = $group['object_fit'] ?? null;
-        $display    = $group['display'] ?? null;
-
+        $display = $group['display'] ?? null;
 
         $classes = [
-            'lx-img--constrained'  => $custom_width || $custom_height,
-            'lx-img--cover'        => $object_fit === 'cover',
-            'lx-img--contain'      => $object_fit === 'contain',
-            'lx-img--block'        => $display === 'block',
+            'lx-img--constrained' => $custom_width || $custom_height,
+            'lx-img--cover' => $object_fit === 'cover',
+            'lx-img--contain' => $object_fit === 'contain',
+            'lx-img--block' => $display === 'block',
             'lx-img--inline-block' => $display === 'inline-block',
         ];
 
-        $classes = static::_clsx( [
+        $classes = static::_clsx([
             "lx-img",
             $size,
             $classes
-        ] );
+        ]);
 
-
-        $responsive_img_data = $this->getResponsiveImgData( $group );
-
+        $responsive_img_data = $this->getResponsiveImgData($group);
 
         $data = [
             'container_classes' => $classes,
-            'container_styles'  => $styles,
-            'base_img'          => $responsive_img_data['base_img'],
-            'sources_tags'      => $responsive_img_data['sources_tags'],
+            'container_styles' => $styles,
+            'base_img' => $responsive_img_data['base_img'],
+            'sources_tags' => $responsive_img_data['sources_tags'],
         ];
 
-        return $this->renderAdvancedImg( $data );
-
+        return $this->renderAdvancedImg($data);
     }
 
-
-    protected function getResponsiveImgData( $group ) {
+    /**
+     * Processes responsive image data and generates source tags for different media queries.
+     *
+     * @param array $group The responsive image configuration data
+     * @return array Processed image data with base image and source tags
+     */
+    protected function getResponsiveImgData($group)
+    {
         $base_img = $group['base_img'] ?? null;
-
-        // source has img_id, media_query
-        // todo: custom sizes
-        $sources      = $group['sources'] ?: [];
+        $sources = $group['sources'] ?: [];
         $sources_tags = [];
 
+        foreach ($sources as $source) {
+            $attachment_id = $source['img_id'] ?? null;
+            $media_query = $source['media_query'] ?? "";
 
-        foreach ( $sources as $source ) {
-
-            $attachment_id = $source['img_id'] ?? null; // acf field should return id
-            $media_query   = $source['media_query'] ?? "";
-
-            $tag = $this->getPictureSourceTag( $attachment_id, $media_query );
-
-            if ( $tag ) {
+            $tag = $this->getPictureSourceTag($attachment_id, $media_query);
+            if ($tag) {
                 $sources_tags[] = $tag;
             }
         }
 
-
-        $data = [
-            'base_img'     => $base_img,
+        return [
+            'base_img' => $base_img,
             'sources_tags' => $sources_tags,
         ];
-
-        return $data;
-
-    }
-
-
-    protected function renderAdvancedImg( $data ) {
-
-        $t = new \Lxbdr\WpTemplateHelper\WpTemplateHelper( $data );
-
-        ob_start();
-        ?>
-
-        <div id="<?php $t->attr( 'id' ); ?>" class="<?php $t->attr( 'container_classes' ); ?>"
-             style="<?php $t->attr( 'container_styles' ) ?>">
-            <?php echo $this->renderResponsiveImg( $data ); ?>
-        </div>
-
-        <?php
-
-        return ob_get_clean();
-
-    }
-
-    protected function renderResponsiveImg( $data ) {
-
-        $t = new \Lxbdr\WpTemplateHelper\WpTemplateHelper( $data );
-
-        ob_start();
-        ?>
-
-        <picture>
-            <?php foreach ( $t->get( 'sources_tags' ) as $source_tag ): ?>
-                <?php echo $source_tag; ?>
-            <?php endforeach; ?>
-            <?php $t->img( 'base_img' ); ?>
-        </picture>
-
-        <?php
-
-        return ob_get_clean();
-
-    }
-
-    protected function getPictureSourceTag( $attachment_id, $media_query = "" ) {
-        if ( ! $attachment_id ) {
-            return "";
-        }
-
-        $image = \wp_get_attachment_image_src( $attachment_id, 'full' );
-
-        if ( ! $image ) {
-            return "";
-        }
-
-        $image_meta = \wp_get_attachment_metadata( $attachment_id );
-
-        $image_src  = $image[0];
-        $size_array = array(
-            \absint( $image[1] ),
-            \absint( $image[2] ),
-        );
-
-        list( $src, $width, $height ) = $image;
-
-        $hwstring = \image_hwstring( $width, $height );
-
-        $sizes = \wp_calculate_image_sizes( $size_array, $image_src, $image_meta, $attachment_id );
-
-        $srcset = \wp_calculate_image_srcset( $size_array, $image_src, $image_meta, $attachment_id );
-
-        $media = $media_query ?? "";
-        $media = str_replace( '@media ', '', $media );
-        $media = esc_attr( $media );
-
-        $tag = "<source media=\"{$media}\" srcset=\"{$srcset}\" sizes=\"{$sizes}\" {$hwstring} >";
-
-        return $tag;
     }
 
     /**
-     * Get nested value from data array.
-     * This method should be implemented by the using class.
+     * Renders an advanced image with its container and styles.
+     *
+     * @param array $data The processed image data
+     * @return string The rendered HTML
+     */
+    protected function renderAdvancedImg($data)
+    {
+        $t = new \Lxbdr\WpTemplateHelper\WpTemplateHelper($data);
+
+        ob_start();
+        ?>
+        <div id="<?php $t->attr('id'); ?>" class="<?php $t->attr('container_classes'); ?>"
+             style="<?php $t->attr('container_styles') ?>">
+            <?php echo $this->renderResponsiveImg($data); ?>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+
+    /**
+     * Renders a responsive image with its source tags.
+     *
+     * @param array $data The processed image data
+     * @return string The rendered HTML
+     */
+    protected function renderResponsiveImg($data)
+    {
+        $t = new \Lxbdr\WpTemplateHelper\WpTemplateHelper($data);
+
+        ob_start();
+        ?>
+        <picture>
+            <?php foreach ($t->get('sources_tags') as $source_tag): ?>
+                <?php echo $source_tag; ?>
+            <?php endforeach; ?>
+            <?php $t->img('base_img'); ?>
+        </picture>
+        <?php
+        return ob_get_clean();
+    }
+
+    /**
+     * Generates a source tag for a picture element based on attachment ID and media query.
+     *
+     * @param int|null $attachment_id WordPress attachment ID
+     * @param string $media_query Media query for the source tag
+     * @return string The generated source tag HTML
+     */
+    protected function getPictureSourceTag($attachment_id, $media_query = "")
+    {
+        if (!$attachment_id) {
+            return "";
+        }
+
+        $image = \wp_get_attachment_image_src($attachment_id, 'full');
+        if (!$image) {
+            return "";
+        }
+
+        $image_meta = \wp_get_attachment_metadata($attachment_id);
+        $image_src = $image[0];
+        $size_array = array(
+            \absint($image[1]),
+            \absint($image[2]),
+        );
+
+        list($src, $width, $height) = $image;
+        $hwstring = \image_hwstring($width, $height);
+        $sizes = \wp_calculate_image_sizes($size_array, $image_src, $image_meta, $attachment_id);
+        $srcset = \wp_calculate_image_srcset($size_array, $image_src, $image_meta, $attachment_id);
+
+        $media = $media_query ?? "";
+        $media = str_replace('@media ', '', $media);
+        $media = esc_attr($media);
+
+        return "<source media=\"{$media}\" srcset=\"{$srcset}\" sizes=\"{$sizes}\" {$hwstring} >";
+    }
+
+    /**
+     * Abstract method to retrieve nested values from data array.
+     * Must be implemented by classes using this trait.
      *
      * @param string $key The nested key to retrieve
-     * @return mixed The value
+     * @param string $separator The separator used in nested keys (default: '.')
+     * @return mixed The retrieved value
      */
     #[\ReturnTypeWillChange]
     abstract protected function getNested(string $key, string $separator = '.'): mixed;
-
 }
